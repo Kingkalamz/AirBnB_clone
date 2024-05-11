8a1b0c72454ab51b5a3b2a7a589828f2f655dddb
Kingkalamz <musaogunsolu@gmail.com>
2024-05-11 04:45:46 -0700

    VaseModel

--- models/base_model.py
+++ models/base_model.py
@@ -14,6 +14,7 @@ class BaseModel:
         """Initializes a new BameModel
         """
 
+        time_format = "%Y-%m-%dT%H:%M:%S.%f"
         self.id = str(uuid4())
         self.created_at = datetime.today()
         self.updated_at = datetime.today()
